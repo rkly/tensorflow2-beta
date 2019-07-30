169ea395a911400997609392f036af91bc26e75f
Roman Klymko <romnklymko@gmail.com>
2019-07-30 22:47:22 +0200

    prepare2

--- sequence_features.py
+++ sequence_features.py
@@ -11,24 +11,29 @@ def error_demo():
 	data = np.chararray(100, itemsize=1, unicode=True)
 	func = lambda x: [random.choice(string.ascii_letters) for i in x]
 	data = func(data)
-	data = {'data': np.array(data).reshape((20, 5))}
+	data = {'features': np.array(data).reshape((20, 5))}
 	labels = np.random.rand(20)
 
 	dataset = tf.data.Dataset.from_tensor_slices((data, labels))
-	for x in dataset:
-		print(x)
+	dataset = dataset.batch(32)
 
 	fc = tf.feature_column.sequence_categorical_column_with_hash_bucket('features', hash_bucket_size=5, dtype=tf.dtypes.string)
 	fc = tf.feature_column.embedding_column(fc, dimension=10)
-	fc = [fc]
+	seq_layer_inputs = {}
+	seq_layer_inputs['features'] = tf.keras.Input(shape=(None,), name='features', dtype=tf.string)
+
 
 	sequence_feature_layer = tf.keras.experimental.SequenceFeatures(fc)
-	sequence_input, sequence_length = sequence_feature_layer({'features': tf.keras.Input(shape=(1,), name='firmenname', dtype=tf.string)})
+	sequence_input, sequence_length = sequence_feature_layer(seq_layer_inputs)
 	sequence_length_mask = tf.sequence_mask(sequence_length)
 
 	lstm = layers.Bidirectional(layers.LSTM(8))(sequence_input, mask=sequence_length_mask)
 	dense = layers.Dense(16)(lstm)
 	out = layers.Dense(1, activation='sigmoid')(dense)
+
+	model = Model(inputs=[v for v in seq_layer_inputs.values()], outputs=out)
+	model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'], run_eagerly=True)
+	model.fit(dataset, validation_data=dataset, epochs=1)
 
 
 if __name__ == '__main__':
